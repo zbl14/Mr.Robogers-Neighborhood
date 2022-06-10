1853841915ad5423e0dc49b77c58415abd9c6f80
zbl14 <ifthereisoneday@gmail.com>
2022-06-10 13:20:39 -0700

    undate countBy function, the return will be an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+// Utility Logic
+function noInputtedWord() {
+  for (let i=0; i < arguments.length; i++) {
+    if (arguments[i].trim().length === 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
 // Business Logic
 function countBy(num) {
   if(Number(num) && num>0) {
@@ -12,28 +22,40 @@ function countBy(num) {
         countArray[i]=("Beep!");
       }
     }
-    return countArray.join(" ");
+    return countArray;
   } else {
     return false;
   }
 }
 
 // UI Logic
+function msgWithName(num) {
+  // if (noInputtedWord(num)) {
+  //   return "";
+  // }
+  let htmlStr = "<p>";
+  let msgArray = countBy(num).split(" ");
+  msgArray.forEach(function(element) {
+    if (element === "Won't you be my neighbor?") {
+      htmlStr = htmlStr.concat("Won't you be my neighbor, XX?");
+    } else {
+      htmlStr = htmlStr.concat(element);
+    }
+  });
+  return htmlStr + "</p>"
+}
+
 $(document).ready(function() {
   $("form#formOne").submit(function(event){
     event.preventDefault();
     const numInput = parseInt($("input#numInput").val());
     const nameInput = $("input#name").val();
-    let numCount = countBy(numInput);
-    $("#result").html(numCount).show();
-    $("#neighborName").html(nameInput).show();
+    // let numCount = countBy(numInput);
+    $("#msgWithName").html(countBy(numInput)).show();
+    // $("#neighborName").html(nameInput).show();
     $("#clearResult").show();
-    // $("#result")
-
-
   });
   $("#clearResult").click(function() {
-    $("#clearResult, #neighborName, #result").hide();
+    $("#clearResult, #msgWithName").hide();
   });
-
 });
